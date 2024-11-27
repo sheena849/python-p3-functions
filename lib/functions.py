544e444844636000ba9775bb788422a44794cc96
sheena849 <sheenamugo04@gmail.com>
2024-11-27 22:32:17 +0300

    Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    print ("Hello, programmer!")
+    print("Hello, programmer!")
 
 def greet(name):
-    print (f"Hello, {name}!")
-greet("Guido")
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print (f"Hello, {name}!")
-greet_with_default()
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
 
 def halve(number):
-    return number/2
+    return number / 2
